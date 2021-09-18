7c65b11bab88af301848712bf954a58013d2bfa0
smilejakdu <ash982416@gmail.com>
2021-09-18 17:38:40 +0900

    baekjoon 2798

--- baekjoon/bronze/2798.py
+++ baekjoon/bronze/2798.py
@@ -1,2 +1,13 @@
-n, m = list(map(int, input().split(" ")))
-print(n)
+N, M = map(int, input().split())
+number_list = list(map(int, input().split()))
+result = []
+
+if N != len(number_list):
+    raise BaseException
+
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            if number_list[i] + number_list[j] + number_list[k] <= M:
+                result.append(number_list[i] + number_list[j] + number_list[k])
+print(max(result))
