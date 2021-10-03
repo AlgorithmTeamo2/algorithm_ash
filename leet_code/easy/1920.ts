652f88f2646092700cc9c93f9dc75f7d2db7def0
smilejakdu <ash982416@gmail.com>
2021-10-03 15:51:33 +0900

    leetcode 1920

--- leet_code/easy/1920.ts
+++ leet_code/easy/1920.ts
@@ -9,3 +9,14 @@ ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]]
     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
     = [0,1,2,4,5,3]
 */
+
+const nums = [0, 2, 1, 5, 3, 4];
+
+function buildArray(nums: number[]) {
+  const numList = nums.map((e) => {
+    return nums[e];
+  });
+  return numList;
+}
+
+console.log(buildArray(nums));
